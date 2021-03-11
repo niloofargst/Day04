d16f04af6c3308eb815fd05141ebbdfc7db9d192
Niloufar Ghesmati <Niloofargst@gmail.com>
2021-03-11 23:49:08 +0100

    adding folders

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+
+    var myDecimal = 5.458
+    var myDecimalE = 254e-5
+    return {
+        myDecimal,
+        myDecimalE
+    };
+
+}
+
+console.log(main());
+module.exports = main;
---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+
+    var myFirstName = "Niloufar";
+    var myLastName = "Ghesmati";
+    var myHomeTown = "Sarajevo";
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
